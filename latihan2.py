6bd02f394f648c77b3c31814bd1d9f225f87dd77
wandasetya <72745059+wandasetya@users.noreply.github.com>
2020-11-07 21:10:26 +0700

    Create latihan2.py

---
+++ latihan2.py
@@ -0,0 +1,10 @@
+print ('menampilkan bilangan terbesar dari n buah data yang di inputkan')
+
+max = 0
+while true:
+    a = int(input("masukan bilangan :")
+    if max < a :
+        max = a
+    if a == 0 :
+         break
+print(" bilangan terbesar adalah ", max)
